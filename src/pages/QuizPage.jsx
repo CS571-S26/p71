5507c84de0b82b02191cba933732dc9695d96716
Avery Schwalbe <aschwalbe19@gmail.com>
2026-04-22 22:44:33 -0500

    checkpoint 3 interaction

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -10,10 +10,39 @@ export default function QuizPage(){
                 <Button variant="primary" onClick={() => alert("Correct!")}>d4</Button>
                 <Button variant="success" onClick={() => alert("Incorrect!")}>e4</Button>
                 <Button variant="warning" onClick={() => alert("Incorrect!")}>h4</Button>
-                <Button variant="danger" onClick={() => alert("Incorrect!!")}>c3</Button>
+                <Button variant="danger" onClick={() => alert("Incorrect!")}>c3</Button>
             
             
             </Card> 
+                        <Card>
+                <h1>What move can the sicilian defense be played against</h1>
+                <Button variant="primary" onClick={() => alert("Incorrect!")}>d4</Button>
+                <Button variant="success" onClick={() => alert("Correct!")}>e4</Button>
+                <Button variant="warning" onClick={() => alert("Incorrect!")}>h4</Button>
+                <Button variant="danger" onClick={() => alert("Incorrect!")}>c3</Button>
+            
+            
+            </Card> 
+                <Card>
+                <h1>What is the first move the sicilian defense?</h1>
+                <Button variant="primary" onClick={() => alert("Incorrect!")}>e5</Button>
+                <Button variant="success" onClick={() => alert("Incorrect!")}>d6</Button>
+                <Button variant="warning" onClick={() => alert("Incorrect!")}>a5</Button>
+                <Button variant="danger" onClick={() => alert("Correct!")}>c5</Button>
+            
+            
+            </Card> 
+
+            <Card>
+                <h1>What isn't a variation of the sicilian defense?</h1>
+                <Button variant="primary" onClick={() => alert("Incorrect!")}>open variation</Button>
+                <Button variant="success" onClick={() => alert("Incorrect!")}>closed variation</Button>
+                <Button variant="warning" onClick={() => alert("Incorrect!")}>Smith-Morra Gambit</Button>
+                <Button variant="danger" onClick={() => alert("Correct!")}>Krause variation</Button>
+            
+            
+            </Card> 
+
         <p>More questions coming soon!</p>
         </div>
     )
--- src/pages/SicilianLessonPage.jsx
+++ src/pages/SicilianLessonPage.jsx
@@ -5,7 +5,20 @@ export default function SicilianLessonPage(){
     return(
         <div>
             <ChessBoard/>
-            <p>Learning how to play the Sicilian defense will come soon!</p>
+            <h1>This page is for learning the Sicilian defense! </h1>
+            <br></br>
+            <p>The Sicilian defense is a type a defensive opening while playing with the black pieces.</p>
+            <p>The defense starts in response to the opponent moving their pawn from positon e2 to e4</p>
+            <p>The crucial responding move played with the sicilian defense is the c7 pawn to c5</p>
+            <p>This starting position has multiple continuing variations called lines.</p>
+            <br></br>
+            <p>For example, one variation is the open variation.</p>
+            <p>This occurs when the opponent brings their knight from g1 to f3</p>
+            <p>The resulting move is you playing pwn from d7 to d6. The most common follow up is the opponent moving their pawn from d2 to d4</p>
+            <p>You should take the pawn on d4 with your pawn thta is on c5, and the opponent will most likely capture your resulting pawn with their knight</p>
+            <p>This is a strong defense with the black pieces.</p>
+            <p>The opponent has space occupied in the center of the board which is considered the ideal for openings, however this allows you to occupy space with the light square bishop located on c8</p>
+            
         </div>
     )
 }
