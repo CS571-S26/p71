157814ace8862f7da0976068447a3d5386492177
Avery Schwalbe <aschwalbe19@gmail.com>
2026-04-29 21:42:49 -0500

    fixed tips and lesson card

--- src/components/LessonCard.jsx
+++ src/components/LessonCard.jsx
@@ -1,11 +1,11 @@
 import { Card } from 'react-bootstrap'
 
-export default function LessonCard({ title, children }) {
+export default function LessonCard({ title,text, children }) {
   return (
     <Card className="shadow-sm border-0 rounded-4 mb-4">
       <Card.Body>
         <h2 className="h4 mb-3">{title}</h2>
-        <div className="text-muted">{children}</div>
+        <div className="text-muted">{text || children}</div>
       </Card.Body>
     </Card>
   )
--- src/components/tips.jsx
+++ src/components/tips.jsx
@@ -1,9 +1,9 @@
 import { Alert } from 'react-bootstrap'
 
-export default function Tips({text}){
+export default function Tips({children}){
     return(
         <Alert variant="info" className="rounded-4">
-            <strong>Tip:</strong> {text}
+            <strong>Tip:</strong> {children}
         </Alert>
     )
 }
